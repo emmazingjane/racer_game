9192cd02518e0e202d53ab065d8037774eaf0b17
emmabennett <emma.j.bennett1015@gmail.com>
2018-02-02 08:30:31 -0800

    added timer for star button

--- java.js
+++ java.js
@@ -45,7 +45,29 @@ function setWinState(car){
 
 // when the page selects a winner, how far along the page
 
+function countdown(minutes) {
+    var seconds = 60;
+    var mins = minutes
+    function tick() {
+        //This script expects an element with an ID = "counter". You can change that to what ever you want. 
+        var counter = document.getElementById("counter");
+        var current_minutes = mins-1
+        seconds--;
+        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else {
+            if(mins > 1){
+                countdown(mins-1);           
+            }
+        }
+    }
+    tick();
+}
 
+countdown(1);
+
+// added counter to start  button
 
 
 
